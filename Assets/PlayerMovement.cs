4bfd41ceb4b24709dbb2675597ca12df8dedf1d4
v5slv <valerie5slv@gmail.com>
2024-02-09 14:38:56 +0100

    Added player animations (idle and running)

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,23 +6,49 @@ public class PlayerMovement : MonoBehaviour
 
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float dirX = 0f;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         Debug.Log("Coucou");
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, 14f);
+        }
+
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
 }
