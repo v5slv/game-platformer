aa575bfaef202f999bf396f240725959ffc366eb
v5slv <valerie5slv@gmail.com>
2024-03-08 14:16:20 +0100

    Added counter, traps (spikes) and player death

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
-   private void OnTriggerEnter2D(Collider2D collision)
-   {
-    if (collision.gameObject.CompareTag("Strawberry"))
+    private int strawbs = 0;
+
+    [SerializeField] private TextMeshProUGUI strawbsText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(collision.gameObject);
-    }
+        if (collision.gameObject.CompareTag("Strawberry"))
+        {
+            Destroy(collision.gameObject);
+            strawbs++;
+            strawbsText.text = "Strawberries : "+strawbs;
+        }
    }
 }
